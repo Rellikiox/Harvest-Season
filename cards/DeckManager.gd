1dc5fbdfd75b13d6edf07db787f88c4d4edfc2ad
Martín Candela Calabuig <rellikiox@gmail.com>
2020-04-18 19:52:41 +0100

    Add draw pile, hand and discard pile
    - Deck management now:
    -- Initializes a draw_pile from the deck
    -- Deals cards into the hand form the draw_pile
    -- Discarded cards are shuffled into the draw pile when it's empty

--- cards/DeckManager.gd
+++ cards/DeckManager.gd
@@ -3,20 +3,51 @@ extends Node
 
 var deck = [
 	Global.CardEnum.PLANT_BEET,
+	Global.CardEnum.PLANT_BEET,
+	Global.CardEnum.PLANT_BEET,
+	Global.CardEnum.PLANT_PEAS,
+	Global.CardEnum.PLANT_PEAS,
+	Global.CardEnum.PLANT_PEAS,
+	Global.CardEnum.PLANT_CABBAGE,
+	Global.CardEnum.PLANT_CABBAGE,
 	Global.CardEnum.PLANT_CABBAGE,
 	Global.CardEnum.PLANT_ONION,
-	Global.CardEnum.PLANT_PEAS,
-	Global.CardEnum.PLANT_POTATO
+	Global.CardEnum.PLANT_ONION,
+	Global.CardEnum.PLANT_ONION,
+	Global.CardEnum.PLANT_POTATO,
+	Global.CardEnum.PLANT_POTATO,
+	Global.CardEnum.PLANT_POTATO,
+	Global.CardEnum.PLACE_SPRINKLER,
+	Global.CardEnum.PLACE_SPRINKLER,
+	Global.CardEnum.DIG_GROUND,
+	Global.CardEnum.DIG_GROUND,
+	Global.CardEnum.DIG_GROUND
 ]
-var index = 0
 
+var draw_pile = []
+var hand = []
+var discard_pile = []
 
 func _init():
-	print('asd')
+	randomize()
+	draw_pile = deck.duplicate()
+	draw_pile.shuffle()
 
 
 func draw_cards(amount):
-	var cards = deck.slice(index, index + amount - 1)
-	index = (index + amount) % len(deck)
-	return cards
+	for card in hand:
+		discard_pile.append(card)
+		
+	hand = []
+	for i in range(amount):
+		if draw_pile.empty():
+			replenish_draw_pile()
+		hand.append(draw_pile.pop_front())
 	
+	return hand
+	
+	
+func replenish_draw_pile():
+	draw_pile = discard_pile
+	draw_pile.shuffle()
+	discard_pile = []
